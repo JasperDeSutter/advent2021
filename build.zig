c3d7169a0bf5be6eb6b0c4b4b04dd63822db25fa
JasperDeSutter <jasper.desutter@gmail.com>
2021-12-02 19:13:01 +0100

    add build support for multiple days

--- build.zig
+++ build.zig
@@ -3,22 +3,37 @@ const builtin = std.builtin;
 
 pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
-
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("day01", "src/day01.zig");
-    
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
-    
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
 
-    const run_step = b.step("day01", "Run day01");
-    run_step.dependOn(&run_cmd.step);
+    const test_step = b.step("test", "Run all tests");
 
-    const input_path = b.pathFromRoot("inputdata/day01.txt");
-    const file = try std.fs.openFileAbsolute(input_path, .{ .read = true });
-    const input = try file.readToEndAlloc(b.allocator, 10_000_000);
-    run_cmd.addArg(input);
+    var day: u32 = 1;
+    var end: u32 = 2;
+    while (day <= end) : (day += 1) {
+        var dayStringBuf: [5]u8 = undefined;
+        const dayString = try std.fmt.bufPrint(dayStringBuf[0..], "day{:0>2}", .{day});
+
+        const srcFile = b.fmt("src/{s}.zig", .{dayString});
+
+        const exe = b.addExecutable(dayString, srcFile);
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
+        exe.install();
+
+        const run_cmd = exe.run();
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        const run_step = b.step(dayString, b.fmt("Run {s}", .{dayString}));
+        run_step.dependOn(&run_cmd.step);
+
+        const input_path = b.pathFromRoot(b.fmt("inputdata/{s}.txt", .{dayString}));
+        const file = try std.fs.openFileAbsolute(input_path, .{ .read = true });
+        const input = try file.readToEndAlloc(b.allocator, 10_000_000);
+        run_cmd.addArg(input);
+
+        const test_cmd = b.addTest(srcFile);
+        test_cmd.setTarget(target);
+        test_cmd.setBuildMode(mode);
+        test_step.dependOn(&test_cmd.step);
+    }
 }
