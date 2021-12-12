840baad3d4357bd7bec4a88b606ee0bf2750eba2
JasperDeSutter <jasper.desutter@gmail.com>
2021-12-12 10:08:44 +0100

    day08: solve part 2

--- src/day08.zig
+++ src/day08.zig
@@ -7,54 +7,100 @@ pub fn main() anyerror!void {
 }
 
 fn solve(_: *std.mem.Allocator, input: []const u8) anyerror!void {
-    const result = try countSimpleDigits(input);
-    std.debug.print("simple digits: {d}\n", .{result});
+    const result = try retrieveDigits(input);
+    std.debug.print("simple digits: {d}\n", .{result.simple_digit_count});
+    std.debug.print("value sum: {d}\n", .{result.values_sum});
 }
 
-const Digits = [10](?[]const u8);
+const Digit = ?[]const u8;
 
 const Solution = struct {
-    digits: Digits,
+    seven: u8,
+    four: u8,
 
-    const simple_digits = [_]u8{1, 4, 7, 8};
-    fn countSimpleDigits(self: *const @This()) u8 {
-        var iter = utils.iter(u8, simple_digits[0..]);
-        var sum:u8 = 0;
-        while (iter.next()) |it| {
-            const digit = self.digits[it.*];
-            if (digit != null) {
-                std.debug.print("{s}\n", .{digit});
-                sum += 1;
-            }
+    fn init() @This() {
+        return .{ .seven = 0, .four = 0 };
+    }
+
+    fn transformDigit(part: []const u8) u8 {
+        var bitset = std.bit_set.StaticBitSet(7).initEmpty();
+        for (part) |char| {
+            bitset.set(char - 'a');
         }
-        return sum;
+        return bitset.mask;
+    }
+
+    fn add_digit(self: *@This(), part: []const u8) void {
+        switch (part.len) {
+            3 => {
+                self.seven = transformDigit(part);
+            },
+            4 => {
+                self.four = transformDigit(part);
+            },
+            else => {},
+        }
+    }
+
+    fn getDigit(self: *const @This(), part: []const u8) u8 {
+        return switch (part.len) {
+            2 => 1,
+            3 => 7,
+            4 => 4,
+            5 => {
+                const value = transformDigit(part);
+                if ((value & self.seven) == self.seven) return 3;
+                return if (@popCount(u8, (value & self.four)) == 2) 2 else 5;
+            },
+            6 => {
+                const value = transformDigit(part);
+                if ((value & self.seven) != self.seven) return 6;
+                return if ((value & self.four) == self.four) 9 else 0;
+            },
+            7 => 8,
+            else => @panic(part),
+        };
     }
 };
 
-fn countSimpleDigits(input: []const u8) anyerror!u16 {
-    var total: u16 = 0;
+const Result = struct {
+    simple_digit_count: u16,
+    values_sum: u32,
+};
+
+fn retrieveDigits(input: []const u8) anyerror!Result {
+    var result = Result{ .values_sum = 0, .simple_digit_count = 0 };
     var lines = std.mem.split(u8, input, "\n");
     while (lines.next()) |line| {
         const bar = std.mem.indexOf(u8, line, "|").?;
-        var parts = std.mem.split(u8, line[(bar + 1)..], " ");
-        // var digits: Digits = .{null} ** 10;
-        while (parts.next()) |part| {
-            const digit: ?u8 = switch(part.len) {
-                2 => 1,
-                3 => 7,
-                4 => 4,
-                7 => 8,
-                else => null,
-            };
-            if (digit != null) {
-                // digits[digit.?] = part;
-                total += 1;
+        const solution = b: {
+            var solution = Solution.init();
+            var parts = std.mem.split(u8, line[0..(bar - 1)], " ");
+            while (parts.next()) |part| {
+                solution.add_digit(part);
             }
+            break :b solution;
+        };
+
+        {
+            var parts = std.mem.split(u8, line[(bar + 2)..], " ");
+            var number: u16 = 0;
+            while (parts.next()) |part| {
+                const digit = solution.getDigit(part);
+                number = number * 10 + digit;
+
+                const simple_digit = switch (digit) {
+                    1, 4, 7, 8 => true,
+                    else => false,
+                };
+                if (simple_digit) {
+                    result.simple_digit_count += 1;
+                }
+            }
+            result.values_sum += number;
         }
-        // const solution = Solution { .digits = digits };
-        // total += solution.countSimpleDigits();
     }
-    return total;
+    return result;
 }
 
 test {
@@ -71,6 +117,7 @@ test {
         \\gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     ;
 
-    const result1 = try countSimpleDigits(input);
-    try std.testing.expectEqual(@intCast(u16, 26), result1);
+    const result = try retrieveDigits(input);
+    try std.testing.expectEqual(@intCast(u16, 26), result.simple_digit_count);
+    try std.testing.expectEqual(@intCast(u32, 61229), result.values_sum);
 }
