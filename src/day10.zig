466adaaadd5ca891ae7e4e9f304da08ab8630365
JasperDeSutter <jasper.desutter@gmail.com>
2022-04-13 17:42:12 +0200

    day10: comptime config of points per character

--- src/day10.zig
+++ src/day10.zig
@@ -12,25 +12,33 @@ fn solve(alloc: *std.mem.Allocator, input: []const u8) anyerror!void {
     std.debug.print("autocomplete score: {d}\n", .{result[1]});
 }
 
+const CharConfig = struct {
+    open: u8,
+    close: u8,
+    illegalPoints: u64,
+    autocompletePoints: u64,
+};
+
+const config = [_]CharConfig{
+    CharConfig{ .open = '(', .close = ')', .illegalPoints = 3, .autocompletePoints = 1 },
+    CharConfig{ .open = '[', .close = ']', .illegalPoints = 57, .autocompletePoints = 2 },
+    CharConfig{ .open = '{', .close = '}', .illegalPoints = 1197, .autocompletePoints = 3 },
+    CharConfig{ .open = '<', .close = '>', .illegalPoints = 25137, .autocompletePoints = 4 },
+};
+
 fn isClose(char: u8) bool {
-    return switch (char) {
-        ')', '}', ']', '>' => true,
-        else => false,
-    };
+    inline for (config) |c| {
+        if (char == c.close) return true;
+    }
+    return false;
 }
 
 fn opositeChar(char: u8) u8 {
-    return switch (char) {
-        '(' => ')',
-        '[' => ']',
-        '{' => '}',
-        '<' => '>',
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-        '>' => '<',
-        else => @panic("unexpected input"),
-    };
+    inline for (config) |c| {
+        if (char == c.close) return c.open;
+        if (char == c.open) return c.close;
+    }
+    @panic("unexpected input");
 }
 
 fn firstInvalidChar(copy: *[]const u8) ?u8 {
@@ -75,13 +83,10 @@ fn autocompleteLine(line: []const u8) u64 {
                 }
             }
         } else {
-            const points: u64 = switch (char) {
-                '(' => 1,
-                '[' => 2,
-                '{' => 3,
-                '<' => 4,
-                else => @panic("unexpected input"),
-            };
+            var points: u64 = 0;
+            inline for (config) |c| {
+                if (c.open == char) points = c.autocompletePoints;
+            }
             score = score * 5 + points;
         }
     }
@@ -97,14 +102,9 @@ fn totalSyntaxErrorAndMiddleScore(alloc: *std.mem.Allocator, input: []const u8) 
     while (lines.next()) |line| {
         var copy = line;
         if (firstInvalidChar(&copy)) |invalid| {
-            const points: u64 = switch (invalid) {
-                ')' => 3,
-                ']' => 57,
-                '}' => 1197,
-                '>' => 25137,
-                else => @panic("unexpected input"),
-            };
-            totalError += points;
+            inline for (config) |c| {
+                if (c.close == invalid) totalError += c.illegalPoints;
+            }
         } else {
             const score = autocompleteLine(line);
             scores.append(score) catch @panic("oom");
