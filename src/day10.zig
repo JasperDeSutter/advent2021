170ab85d0b52c38fc66eff5559a9dde39c8dcc55
JasperDeSutter <jasper.desutter@gmail.com>
2022-04-13 17:25:02 +0200

    day10: solve part 2

--- src/day10.zig
+++ src/day10.zig
@@ -6,9 +6,10 @@ pub fn main() anyerror!void {
     try runner.run(solve);
 }
 
-fn solve(_: *std.mem.Allocator, input: []const u8) anyerror!void {
-    const sytaxError = totalSyntaxError(input);
-    std.debug.print("total syntax error: {d}\n", .{sytaxError});
+fn solve(alloc: *std.mem.Allocator, input: []const u8) anyerror!void {
+    const result = totalSyntaxErrorAndMiddleScore(alloc, input);
+    std.debug.print("total syntax error: {d}\n", .{result[0]});
+    std.debug.print("autocomplete score: {d}\n", .{result[1]});
 }
 
 fn isClose(char: u8) bool {
@@ -18,17 +19,25 @@ fn isClose(char: u8) bool {
     };
 }
 
-fn firstInvalidChar(copy: *[]const u8) ?u8 {
-    var line = copy.*;
-    if (line.len < 1) return null;
-
-    const oposite: u8 = switch (line[0]) {
+fn opositeChar(char: u8) u8 {
+    return switch (char) {
         '(' => ')',
         '[' => ']',
         '{' => '}',
         '<' => '>',
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+        '>' => '<',
         else => @panic("unexpected input"),
     };
+}
+
+fn firstInvalidChar(copy: *[]const u8) ?u8 {
+    var line = copy.*;
+    if (line.len < 1) return null;
+
+    const oposite = opositeChar(line[0]);
     line = line[1..];
 
     while (line.len > 0) {
@@ -44,23 +53,68 @@ fn firstInvalidChar(copy: *[]const u8) ?u8 {
     return null;
 }
 
-fn totalSyntaxError(input: []const u8) u32 {
-    var lines = std.mem.split(u8, input, "\n");
+fn autocompleteLine(line: []const u8) u64 {
+    var score: u64 = 0;
+    var i = line.len - 1;
+    while (i < std.math.maxInt(u64)) : (i -%= 1) {
+        const char = line[i];
+        if (isClose(char)) {
+            var matches: u8 = 1;
+            const oposite = opositeChar(char);
+            i -= 1;
+            while (i < std.math.maxInt(u64)) : (i -%= 1) {
+                const char2 = line[i];
+                if (char2 == char) {
+                    matches += 1;
+                }
+                if (char2 == oposite) {
+                    matches -= 1;
+                    if (matches == 0) {
+                        break;
+                    }
+                }
+            }
+        } else {
+            const points: u64 = switch (char) {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                else => @panic("unexpected input"),
+            };
+            score = score * 5 + points;
+        }
+    }
+    return score;
+}
 
-    var total: u32 = 0;
+fn totalSyntaxErrorAndMiddleScore(alloc: *std.mem.Allocator, input: []const u8) [2]u64 {
+    var lines = std.mem.split(u8, input, "\n");
+    var scores = std.ArrayList(u64).init(alloc);
+    defer scores.deinit();
+
+    var totalError: u64 = 0;
     while (lines.next()) |line| {
         var copy = line;
-        const invalid = firstInvalidChar(&copy) orelse continue;
-        const points: u32 = switch (invalid) {
-            ')' => 3,
-            ']' => 57,
-            '}' => 1197,
-            '>' => 25137,
-            else => @panic("unexpected input"),
-        };
-        total += points;
+        if (firstInvalidChar(&copy)) |invalid| {
+            const points: u64 = switch (invalid) {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+                else => @panic("unexpected input"),
+            };
+            totalError += points;
+        } else {
+            const score = autocompleteLine(line);
+            scores.append(score) catch @panic("oom");
+        }
     }
-    return total;
+
+    std.sort.sort(u64, scores.items, {}, comptime std.sort.asc(u64));
+    const middle = scores.items[scores.items.len / 2];
+
+    return .{ totalError, middle };
 }
 
 test {
@@ -77,6 +131,7 @@ test {
         \\<{([{{}}[<[[[<>{}]]]>[]]
     ;
 
-    const result1 = totalSyntaxError(input);
-    try std.testing.expectEqual(result1, 26397);
+    const result = totalSyntaxErrorAndMiddleScore(std.testing.allocator, input);
+    try std.testing.expectEqual(result[0], 26397);
+    try std.testing.expectEqual(result[1], 288957);
 }
